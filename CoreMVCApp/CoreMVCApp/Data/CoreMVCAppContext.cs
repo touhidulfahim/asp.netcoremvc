eea53136134844dfa5475e71cc198f83869b6316
touhidulfahim <muhdtohidul@gmail.com>
2019-01-30 00:02:52 +0600

    git commit add movie story(CRUD)

---
+++ CoreMVCApp/CoreMVCApp/Data/CoreMVCAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVCApp.Models
+{
+    public class CoreMVCAppContext : DbContext
+    {
+        public CoreMVCAppContext (DbContextOptions<CoreMVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreMVCApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ CoreMVCApp/CoreMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
